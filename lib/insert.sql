19d7915683562ce9c962140e4a524d6857952f85
Error7799 <kingy7799sentamu@gmail.com>
2024-10-01 14:02:29 +0300

    NICE!

---
+++ lib/insert.sql
@@ -0,0 +1,15 @@
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ("Mr. Chocolate", 10, "M", "Black", "Non-territorial", TRUE);
+
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ("Rowdy", 10, "F", "Black", "Protective of young", FALSE);
+
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ("Tabitha", 10, "M", "Brown", "Social hierarchy", TRUE);
+
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ("Sergeant Brown", 10, "F", "Black", "Fearful of humans", FALSE);
+
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ("Melissa", 10, "M", "Brown", "Non-territorial", TRUE);
+
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ("Grinch", 10, "F", "Black", "Social hierarchy", TRUE);
+
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ("Wendy", 10, "M", "White", "Protective of young", FALSE);
+
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES (NULL, 10, "F", "Black", "Fearful of humans", TRUE);
